51e3f7896c2f71eeacc3270373c0e8a7214818ee
LeeSeoYong6574 <sylee07156574@gmail.com>
2023-04-13 21:48:27 +0900

    aticle write, article list 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
@@ -7,18 +9,62 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
+
 		while (true) {
 			System.out.printf("(명령어)");
 			String cmd = sc.nextLine();
 
 			if (cmd.equals("exit")) {
 				break;
+
 			}
 
+			else if (cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId++;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+
+				articles.add(article);
+
+				System.out.printf("%d번글이 생성되었습니다\n", lastArticleId);
+
+			}if (cmd.equals("article list")) {
+					if (articles.size() == 0) {
+						System.out.println("존재하는 게시물이 없습니다");
+						continue;
+					}
+
+					System.out.println("	번호	|	제목	");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("%d	|	%s	\n", article.id, article.title);
+					}
+				}
+
+
 		}
-		
+
 		sc.close();
-		
+
 		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+
 	}
 }
