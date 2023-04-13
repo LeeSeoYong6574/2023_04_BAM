62baf01489643063f3475e05f9e41e8639f4ecc3
LeeSeoYong6574 <sylee07156574@gmail.com>
2023-04-13 20:37:23 +0900

    명령어

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		System.out.println("== 프로그램 시작 ==");
-		
+
 		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.printf("(명령어)");
+			String cmd = sc.nextLine();
+
+			if (cmd.equals("exit")) {
+				break;
+			}
+
+		}
 		
-		String cmd = sc.nextLine();
-		
-		sc.next();
-		
-		System.out.println(cmd);
+		sc.close();
 		
 		System.out.println("== 프로그램 끝 ==");
 	}
