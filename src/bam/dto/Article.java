c384a42c555d80fad4f7ff6858d57e60b0fd6e85
LeeSeoYong6574 <sylee07156574@gmail.com>
2023-04-20 19:59:55 +0900

    loginedMeber를 Controller로 이전, doWrite에서 작성자 정보 저장

--- src/bam/dto/Article.java
+++ src/bam/dto/Article.java
@@ -3,13 +3,14 @@ package bam.dto;
 public class Article {
 	public int id;
 	public String regDate;
+	public int memberId;
 	public String title;
 	public String body;
-	public int getArticleById;
 	
-	public Article(int id, String regDate, String title, String body) {
+	public Article(int id, String regDate, int memberId, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
+		this.memberId = memberId;
 		this.title = title;
 		this.body = body;
 	}
