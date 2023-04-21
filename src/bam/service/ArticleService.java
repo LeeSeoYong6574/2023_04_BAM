f931498129786a1e4bea524c9d88aef6609a1682
LeeSeoYong6574 <sylee07156574@gmail.com>
2023-04-21 21:31:22 +0900

    ArticleService, ArticleDao, article과 memeber의 id를 Dao로 처리

--- src/bam/service/ArticleService.java
+++ src/bam/service/ArticleService.java
@@ -1,5 +1,39 @@
 package bam.service;
 
-public class ArticleService {
+import java.util.List;
 
+import bam.dao.ArticleDao;
+import bam.dto.Article;
+
+public class ArticleService {
+	
+	private ArticleDao articleDao;
+	
+	public ArticleService() {
+		articleDao = new ArticleDao();
+	}
+
+	public int setLastId() {
+		return articleDao.setLastId();
+	}
+
+	public void add(Article article) {
+		articleDao.add(article);
+	}
+	
+	public List<Article> getArticles(String searchKeyword) {
+		return articleDao.getArticles(searchKeyword);
+	}
+	
+	public Article getArticleById(int id) {
+		return articleDao.getArticleById(id);
+	}
+
+	public void remove(Article foundArticle) {
+		articleDao.remove(foundArticle);
+	}
+
+	public void makeTestData() {
+		articleDao.makeTestData();
+	}
 }
